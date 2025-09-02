ed4c12cc8c2eb10260e806a655f0c63b53a9f06c
Judah Sullivan <judahsullivan.dev@gmail.com>
2025-09-01 20:56:59 -0500

    Fixed Head

--- src/directus/queries/globals.ts
+++ src/directus/queries/globals.ts
@@ -1,34 +1,36 @@
 import { readItem, readItems } from "@directus/sdk";
 import directusClient from "../cli";
 import { getItems } from "./utils";
-
-
-
-
-
+import type { Globals } from "@/types/directusCollection";
 
 export async function getNavigationItems(nav: "main" | "footer" = "main") {
   return directusClient.request(
-    readItem(
-      "navigation",
-      nav,
-      {
-        fields: [
-          {
-            items: [
-              "*",
-              { page: ["permalink"] },
-              {
-                children: [
-                  "*",
-                  { page: ["permalink"] },
-                  { children: ["*", { section: ['*'] }, { page: ["permalink"] }] },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-    )
+    readItem("navigation", nav, {
+      fields: [
+        {
+          items: [
+            "*",
+            { page: ["permalink"] },
+            {
+              children: [
+                "*",
+                { page: ["permalink"] },
+                {
+                  children: ["*", { section: ["*"] }, { page: ["permalink"] }],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    }),
+  );
+}
+
+export async function getGlobals() {
+  return directusClient.request(
+    readItems("globals", {
+      fields: ["*", "favicon.*", "logo.*", "logo_dark_mode.*"],
+    }),
   );
 }
